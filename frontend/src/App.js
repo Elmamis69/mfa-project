93bc58478a1338a2d97d563e93a81a2a4d9db109
Adrián Félix <guerofelix234@gmail.com>
2024-11-04 03:13:53 -0700

    frontend y otros cambios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { React, useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import './App.css';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import TwoFARegister from './pages/TwoFARegister/TwoFARegister';
+import TwoFAVerify from './pages/TwoFAVerify/TwoFAVerify';
+import Main from './pages/Main/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/two-fa-register-page" element={<TwoFARegister />} />
+            <Route exact path="/two-fa-verify-page" element={<TwoFAVerify />} />
+            <Route exact path="/main-page" element={<Main />}/>
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
